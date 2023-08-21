7315afb48044631f5471bb670f15af791919d388
Mikhail Malakhov <etozhenerk@yandex.ru>
2023-08-21 18:42:47 +0300

    fix

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,17 +7,13 @@ const bubbleSort = (arr) => {
   const sortedArr = [...arr];
 
   for (let i = 0; i < sortedArr.length; i++) {
-    const a = sortedArr[i];
-
     for (let j = i; j < sortedArr.length; j++) {
-      const b = sortedArr[j];
-
-      if (b < a) {
-        sortedArr[i] = b;
-        sortedArr[j] = a;
+      if (sortedArr[j] < sortedArr[i]) {
+        const tmp = sortedArr[i];
+        sortedArr[i] = sortedArr[j];
+        sortedArr[j] = tmp;
       }
     }
   }
-
   return sortedArr;
 };
